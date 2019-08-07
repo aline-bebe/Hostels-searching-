5d1b0e08faae71e389aa67c7eb1f97d854230a6b
aline-bebe <ingabirealine40@gmail.com>
2019-08-07 16:01:35 +0200

    some styling added

--- script.js
+++ script.js
@@ -11,3 +11,21 @@ function validateForm() {
         return false;
     }
   }
+
+
+function cycleBackgrounds() {
+var index = 0;
+$imageEls = $('.containere .slide'); // Get the images to be cycled.
+setInterval(function () {
+   // Get the next index.  If at end, restart to the beginning.
+   index = index + 1 < $imageEls.length ? index + 1 : 0;
+   // Show the next
+   $imageEls.eq(index).addClass('show');
+   // Hide the previous
+   $imageEls.eq(index - 1).removeClass('show');
+}, 8000);
+};
+// Document Ready.
+$(function () {
+cycleBackgrounds();
+});
